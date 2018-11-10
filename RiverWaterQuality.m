b3fe6c943acf6d969dce443afa7bb8bad14d2655
Freya Fünfgeld <freyamensch@gmail.com>
2018-11-10 19:15:41 +0100

    continued calculations for river Geometry 1. calculation of H, A_c & q done 2. still to be done: calc of A_s

--- RiverWaterQuality.m
+++ RiverWaterQuality.m
@@ -4,14 +4,9 @@
 % last updated on 27.10.18 at 19:00 by Freya
 
 %% To avoid conflicts: Who is working on which part?
-% Temperature model: Freya
+% River geometry: Freya
+% Radiation Balance: Alireza
 % Oxygen model: Veronica
-
-
-% TEST 2 and 3
-% last try
-% test ali
-%test2 ali 
 
 
 %%
@@ -19,30 +14,47 @@
 %%
 
 
-%% FLOW BALANCE
-%% Flow Balance
-Q =                 % total flow rate [m³/s] - if everywhere the same
-% Neckar in Tübingen ~ 30 m³/s
+%% ---------- RIVER GEOMETRY ----------- 
+%% COMMENTS on Geometry
+% info: so far 3 reaches are computed, all have different hydraulic 
+% parameters, this can be changed to more reaches if liked
+% output parameters: (needed for further calculations) H, A_c, A_s, q
 
-%% Mannings equation, River geometry
-% parameters
-S_river = 0.002;       % bottom slope [m/m]
-n = 0.03;              % Manning roughness coefficient
-B_0 = 3;               % bottom width [m]
-s_bank = 1;          % slope of river banks [m/m]
-H = 2;                  % water depth [m]
-
-% cross-sectional area [m²]
-A_c = (B_0+s_bank*H)*H; 
-
-% wetted perimeter [m]
-P = B_0 + 2*H*sqrt(s_bank^2+1);
-
-% Mannings equation
-Q = (sqrt(S_river)*A_c^(5/3))/(n*P^(2/3))          % total flow rate [m³/s]
+%% PARAMETERS to be adjusted
+Q = 10;                              % volumetric flux [m³/s]       % eg Neckar in Tübingen ~ 30 m³/s
+n = 0.03;                            % Manning roughness coefficient
+ S_river = [0.001 0.001 0.001];       % bottom slope [m/m]
+%S_river = [0.001 0.002 0.004];       % bottom slope [m/m]
+ B_0 = [3 3 3];                       % bottom width [m]
+%B_0 = [2.5 3 4]; 
+s_bank = [1 1 1];                    % slope of river banks [m/m] (dy/dy)
+% s_bank = [1 2 0.5];
+H_0 = 2;                             % initial water depth in first reach
 
 
+%% CALCULATION of water depth
+H = [H_0; zeros(3,1)]';     % creates a vector for water depth [m]
+A_c = zeros(3,1)';          % creates a vector for cross sectional area [m²]
+A_s = zeros(3,1)';          % creates a vector for surface area (water-atmosphere interface) [m²]
+q = zeros(3,1)';            % creates a vector for specific discharge [m/s]
 
+for j=1:3
+for i=2:4
+    % Manning's equation solved for water depth H:    [QUAL2K maual eq(17)]
+    %H(i)= (((Q*n)^(3/5))*(B_0(i-1)+2.*H(i-1)*sqrt(s_bank(i-1).^2+1))^(2/5))/((S_river(i-1)^(3/10)).*(B_0(i-1)+s_bank(i-1).*H(i-1)));
+    % same as above, BUT all H refering to initial height H_0:
+    H(i)= (((Q*n)^(3/5))*(B_0(i-1)+2.*H_0*sqrt(s_bank(i-1).^2+1))^(2/5))/((S_river(i-1)^(3/10)).*(B_0(i-1)+s_bank(i-1).*H_0));
+% Note: the first iteration of H(i-1) refers to the H_0, the following to 
+% the H value calculated in the previous iteration whereas the other 
+% parameters(i-1) all refer to the parameters defined in PARAMETERS section
+end
+A_c(j) = (B_0(j)+s_bank(j)*H(j+1))*H(j+1);     % cross-sectional area [m²] 
+%A_s(j) = ;                                     % surface area (water-atmosphere interface) [m²]
+q(j) = Q./A_c(j);                              % specific discharge [m/s]
+end
+
+
+%%
 %% Longitudinal Dispersion      Manual p.18
 % parameters
 U
