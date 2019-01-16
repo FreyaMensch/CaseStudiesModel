8d8b928aaa1a945fb01a721d05f88c331230aa97
VeronicaCordero <veronica.cordero-perez@student.uni-tuebingen.de>
2019-01-16 12:26:55 +0100

    16_01_19

--- OxygenModel.m
+++ OxygenModel.m
@@ -2,64 +2,59 @@
 %% OxygenModel
 
 %% ///////////// DATA IMPUT ////////////////
+load ('TemperatureModel_14_01_90days.mat','n','q','dx','x','t','dt','Dbulk','A_c','Dm','H','H_G_m','D')
+load ('Tw_14_01_90days.mat')    %(Tw from temperature model)
 c_air = 20.*rand(length(t),1);    % [mg/L]=[g/m³] oxygen concentration in air over time(random) 
 
 %% /////////// TIME LOOP TO CALCULATE OXYGEN CONCENTRATION ///////////////
 %% ///////////////////(with Euler method) ///////////////////////////
-c_in = 10;                 % bc: input concentration at top end of river
-c_x = ones(1,n)*0;        % initial concentration in water (whole river)
-c_xt = ones(length(t),n)*282; % concentration over time and space
+% saturation concentration [g/m3]
+csat = 468./(31.6 + (Tw - 273.15));    %[g/m^3]
+
+c_in = csat(1,1);                 % bc: input concentration at top end of river
+c_x = zeros(1,n);       % initial concentration in water (whole river)
+c_xt = ones(length(t),n); % concentration over time and space
 dcdt_A = zeros(1,n); dcdt_D = zeros(1,n); dcdt_S = zeros(1,n);
 
+c_in_bod = 10;                 % bc: input concentration at top end of river
+c_x_bod = zeros(1,n);        % initial concentration in water (whole river)
+c_xt_bod = ones(length(t),n); % concentration over time and space
+dcdt_A_bod = zeros(1,n); dcdt_D = zeros(1,n); dcdt_S = zeros(1,n);
 for j=1:length(t)
 %========================= ADVECTION ======================================
 dcdt_A = Advectionfun(n,q,dx,c_x,c_in);
-%========================= DISPERSION =====================================
+dcdt_A_bod = Advectionfun(n,q,dx,c_x_bod,c_in_bod);
+% ========================= DISPERSION =====================================
 dcdt_D = Dispersionfun(n,Dbulk,A_c,dx,c_x);
+dcdt_D_bod = Dispersionfun(n,Dbulk,A_c,dx,c_x_bod);
+
 % %========================= EXTERNAL SOURCE ==============================
-% only template - NOT Adjusted yet!!
+%  - NOT Adjusted yet!!
 % for  i=pos:pos             % point source (inserted in only one cell)
-%      W = rho_w*C_p*Q_s*T_s   ;
-%     dcdt_S(i)=W./(rho_w*C_p*A_c(i)*dx);
+%      W = Q_s*c_x   ;
+%     dcdt_S(i)=W./(A_c(i)*dx);
 % end
 % %========================= REACTIONS & MASS TRANSFER ====================
-% % only started - NOT working / finished yet!!
-% dcdt_react=Reactionfun(t,n,H_G_m);
-
-    % photosynthesis rate
-    %F_t=k_20*theta^(T-20);       % Ft: temperature effect limitation
-    
-   
-    theta=1.056 % in general
-    k_20=       % constant at 20 Celsius
-    ke=         % light extintion coeff [1/m]
-    H=0.1       % water depth
-    K_lb=       % bottom algae parameter [w/m2]
-    PAR=PAR_0*(exp(1)^(-ke*H));   % photosynthetically active radiation
-    PAR_0=0.47*H_G;  % photosynthetically active radiation [w/m^2]
-    
-    
-    F_t=k_20*theta^(T-273.15);        % F_t: temperature effect limitation
-    F_l=PAR/(PAR+K_lb);               % F_l: light effect limitation
-
-    r_photo=r_max*F_t*F_l;            % photosynthesis rate
-
-
-    % respiration
-    %r_resp=k_resp_max/h.*c(:,1)./(KO2+c(:,1)).*c(:,2)./(KBOD+c(:,2));%
-    %why divided by h?
-    r_resp=k_resp_max.*(c(:,1)./(KO2+c(:,1)).*c(:,2)./(KBOD+c(:,2))); % need to consider consenctration
-   
-    % gas transfer
-    k2 = 1/h*sqrt(Dm*v/h)*1.0241.^(T-20);
-    %k2 = sqrt(Dm*v/(h^3))*1.0241.^(T-20);
-
-
+% 
+[dcdt_react_O2, dcdt_react_bod] = Reactionfun(t,j,n,H_G_m,c_x,c_x_bod,H,Dm,q,Tw,csat);
 
 %========================= SUM OF ALL =====================================
-c_x = c_x + dcdt_A*dt + dcdt_D*dt + dcdt_S*dt + dcdt_react*dt;
-%T_eq = T_eq + dTdt_A*dt + dTdt_D*dt + dTdt_S*dt ;  % T_eq is calculated by setting all air-water fluxes = 0
-% 
+c_x = c_x + dcdt_A*dt + dcdt_D*dt + dcdt_react_O2*dt ;
+c_x_bod = c_x_bod + dcdt_A_bod*dt + dcdt_D_bod*dt + dcdt_react_bod*dt ;
+
+% + dcdt_S*dt
+
+% figure (1)
+% plot(x,c_x),hold on
+% plot(x,c_x_bod)
+% ylim ([0 20])
+% xlim ([0 30000])
+% hold off
 c_xt(j,:) = c_x;
-%Tw_eq(j,:) = T_eq;
+c_xt_bod(j,:) = c_x_bod ;
 end
+
+%%
+% plot(x,c_xt(120000,:)), hold on;
+% plot(x,c_xt_bod(120000,:))
+% legend
--- Reactionfun.m
+++ Reactionfun.m
@@ -1,21 +1,43 @@
-function dcdt_react = Reactionfun(t,n,H_G_m)
+function [dcdt_react_O2,dcdt_react_bod] = Reactionfun(~,j,n,H_G_m,c_x,c_x_bod,H,Dm,q,Tw,csat)
 %REACTIONFUN Summary of this function goes here
 %   Detailed explanation goes here
+% rate coefficient for photosynthesis
+k_photo = 0.05/86400; % g/(Ws) (problem 16)
+% maximum respiration rate
+k_resp_max = 200/86400; %g/m^2/s  (problem 16)
+% Michaelis-Menten coefficients
+KO2 = 0.5;
+KBOD = 2;
+dcdt_react_O2 = zeros(1,n);
+dcdt_react_bod = zeros(1,n);
 
+     theta=1.056; % temperature coefficient. This has a value of 1.056 in general
+%     k_20=1.104; % BOD rate constant determined at 20oC, day-1
+%     k_e=0.1;    % the light extinction coefficient, [1/m],, from the paper(light coeff coef)page.24 i just put it in the latex leterature,but not mentioned in the text yet!
+%     K_lp= 0.2 ;    % bottom algae parameter [w/m2] !!!
+%     r_max= ;   % the maximum photosynthesis rate,[g/m^2/s]
+    
 for  i=1:n
 % Oxygen input via PHOTOSYNTHESIS
-    k_photo = 10;       % has to be made Temperature dependant!!
-    r_photo = k_photo*H_G_m;
+r_photo = *k_photo*H_G_m(j)./H;  % (problem 16) has to be made Temperature dependant!!
+
+%  k_T = k_20*theta^(Tw(j,i)-273.15);  % F_t: temperature effect limitation
+%  PAR_0 = 0.47*H_G_m(j);              % photosynthetically active radiation [w/m^2]
+%  PAR_z = PAR_0.*exp(-k_e.*H(i));       % photosynthetically active radiation [w/m^2]
+%  F_lp = PAR_z/(PAR_z+K_lp);    % F_l: phytoplankton light effect limitation
+%  r_photo = r_max*k_T*F_lp;     % photosynthesis rate
+
 
 % Oxygen loss via RESPIRATION
-    r_resp = 
+r_resp = (k_resp_max./H).*(c_x(i)./(KO2+c_x(i))).*c_x_bod(i)./(KBOD+c_x_bod(i));
 
 % Oxygen exchange via GAS TRANSFER
-    r_gasTrans =
-    
-    
-% total change of oxygen concentration  over time = Sum of all processes
- dcdt_react(i) = 1;
- %dTdt_R(i) = 1./(H(i)*rho_w*C_p).*(H_insw(i)- H_outsw(i)- H_lwout(i)- H_ET(i) - H_c(i)+ H_lwin(i));
+k2 = sqrt(Dm.*q./(H.^3)).*1.0241.^((Tw(j,i)-273.15)-20);
+r_gas = k2.*(csat(j,i) - c_x(i)) ;
+
+% reaction terms for oxygen and BOD concentrations = Sum of all reaction processes
+ dcdt_react_O2 = r_photo - r_resp + r_gas ;
+ dcdt_react_bod = - r_resp ;
+
 end
 end
